0c90bee6a6dc0c177c02a4ae1f1f0d685c24620b
dzhura <m.dzhura@gmail.com>
2019-06-28 22:41:22 +1000

    Replace Bitmap by vector<bool>

--- problem_1_2/bitmap_sort.cpp
+++ problem_1_2/bitmap_sort.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
 #include "bitmap.h"
 
 int main(int argc, char * argv[])
@@ -7,17 +8,17 @@ int main(int argc, char * argv[])
   size_t N;
   std::cin >> N;
 
-  Bitmap bitmap(N);
+  std::vector<bool> bitmap(N);
 
   while (std::cin.good()) {
     size_t number;
     std::cin >> number;
 
-    bitmap.set_bit(number-1, true);
+    bitmap[number-1] = true;
   }
 
   for (size_t i=0; i<bitmap.size(); ++i) {
-    if (bitmap.get_bit(i)) {
+    if (bitmap[i]) {
       std::cout << i+1 << std::endl;
     }
   }
